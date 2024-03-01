15be3d10ffa55ad6d90d6e0ef94ee732c6f52e1e
Duong Ngoc Duc <137813610+ducdn03@users.noreply.github.com>
2024-03-01 14:57:56 +0700

    still duplicate process without using fork()

--- multiprocess/server/server.c
+++ multiprocess/server/server.c
@@ -8,16 +8,18 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <netdb.h>
+#include <sys/select.h>
 
 #define PORT 8080
 #define SA struct sockaddr
 #define BUFFER_SIZE 1024
+#define MAX_CLIENT 50
 
 void send_list(int connfd)
 {
     DIR *d;
     struct dirent *dir;
-    d = opendir("/home/ducdn/code/ducemb-ssh/multiprocess/server"); // Open the folder
+    d = opendir("."); // Open the folder
     int len;
     char buffer[BUFFER_SIZE];
     memset(buffer, '\0', BUFFER_SIZE);
@@ -86,9 +88,13 @@ int main(void)
     struct sockaddr_in servaddr, cli;
     int sockfd, connfd, len;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    int childpid;
     char buffer[BUFFER_SIZE];
     char file_name[BUFFER_SIZE];
+    int cli_socket[MAX_CLIENT];
+    int acitvity, sd, max_sd;
+    fd_set readfds;
+    int i;
+    memset(cli_socket, 0, sizeof(cli_socket));
     if (sockfd == -1)
     {
         printf("socket creation failed...\n");
@@ -99,6 +105,11 @@ int main(void)
         printf("Socket successfully created..\n");
     }
     bzero(&servaddr, sizeof(servaddr));
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
+    {
+        perror("setsockopt(SO_REUSEADDR) failed");
+        exit(0);
+    }
 
     // assign IP, PORT
     servaddr.sin_family = AF_INET;
@@ -128,54 +139,87 @@ int main(void)
     len = sizeof(cli);
     while (1)
     {
-        connfd = accept(sockfd, (SA *)&cli, &len);
-        if (connfd < 0)
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        max_sd = sockfd;
+        for (i = 0; i < MAX_CLIENT; i++)
         {
-            exit(1);
-        }
-        printf("Connected from %s:%d\n", inet_ntoa(cli.sin_addr), ntohs(cli.sin_port));
-        childpid = fork();
-        if (childpid == 0)
-        {
-            while (1)
+            sd = cli_socket[i];
+            if (sd > 0)
             {
-                close(sockfd);
-                bzero(buffer, BUFFER_SIZE);
-                recv(connfd, buffer, BUFFER_SIZE, 0);
-                printf("From client: %s\n",buffer);
-                if (strncmp(buffer, "exit", 4) == 0)
+                FD_SET(sd, &readfds);
+            }
+            if (sd > max_sd)
+            {
+                max_sd = sd;
+            }
+        }
+        acitvity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        if (acitvity < 0)
+        {
+            printf("Select error\n");
+        }
+        if (FD_ISSET(sockfd, &readfds))
+        {
+            connfd = accept(sockfd, (SA *)&cli, &len);
+            if (connfd < 0)
+            {
+                exit(1);
+            }
+            printf("Connected from %s:%d\n", inet_ntoa(cli.sin_addr), ntohs(cli.sin_port));
+            for (i = 0; i < MAX_CLIENT; i++)
+            {
+                if (cli_socket[i] == 0)
                 {
-                    printf("Disconnected from %s:%d\n", inet_ntoa(cli.sin_addr),\
-                    ntohs(cli.sin_port));
-                    close(connfd);
+                    cli_socket[i] = connfd;
                     break;
                 }
-                else if (strncmp(buffer, "list", 4) == 0)
+            }
+        }
+        for (i = 0; i < MAX_CLIENT; i++)
+        {
+            if (FD_ISSET(cli_socket[i], &readfds))
+            {
+                connfd = cli_socket[i];
+                bzero(buffer, BUFFER_SIZE);
+                if (recv(connfd, buffer, BUFFER_SIZE, 0) > 0)
                 {
-                    send_list(connfd);
-                    bzero(buffer, BUFFER_SIZE);
-                    buffer[0] = '\0';
-                    send(connfd, buffer, BUFFER_SIZE, 0);
-                    printf("Send list successfully...\n");
-                }
-                else
-                {
-                    strcpy(file_name, buffer);
-                    if (file_name[strlen(file_name) - 1] == '\n')
+                    printf("From client: %s\n", buffer);
+                    if (strncmp(buffer, "exit", 4) == 0)
                     {
-                        file_name[strlen(file_name) - 1] = '\0';
+                        printf("Disconnected from %s:%d\n", inet_ntoa(cli.sin_addr),
+                               ntohs(cli.sin_port));
+                        close(connfd);
+                        cli_socket[i] = 0;
                     }
-                    printf("File Name : %s\n", file_name);
-                    bzero(buffer, BUFFER_SIZE);
-                    send_file(file_name, connfd);
-                    bzero(buffer, BUFFER_SIZE);
-                    buffer[0] = '\0';
-                    send(connfd, buffer, BUFFER_SIZE, 0);
-                    printf("Send successfully...\n");
+                    else if (strncmp(buffer, "list", 4) == 0)
+                    {
+                        send_list(connfd);
+                        bzero(buffer, BUFFER_SIZE);
+                        buffer[0] = '\0';
+                        send(connfd, buffer, BUFFER_SIZE, 0);
+                        printf("Send list successfully...\n");
+                    }
+                    else
+                    {
+                        strcpy(file_name, buffer);
+                        if (file_name[strlen(file_name) - 1] == '\n')
+                        {
+                            file_name[strlen(file_name) - 1] = '\0';
+                        }
+                        printf("File Name : %s\n", file_name);
+                        bzero(buffer, BUFFER_SIZE);
+                        send_file(file_name, connfd);
+                        bzero(buffer, BUFFER_SIZE);
+                        buffer[0] = '\0';
+                        send(connfd, buffer, BUFFER_SIZE, 0);
+                        printf("Send successfully...\n");
+                    }
                 }
             }
         }
     }
+    close(sockfd);
     close(connfd);
     return 0;
 }
